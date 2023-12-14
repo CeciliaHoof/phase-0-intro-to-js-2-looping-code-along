4ec8f82f8977a66a4e4f7a15b7d356c156d5f8cd
Cecilia <ccchimento@gmail.com>
2023-12-14 09:03:14 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, event){
+    const cards = [];
+    for ( let x = 0; x < names.length; x++ ) {
+        cards.push(`Thank you, ${names[x]}, for the wonderful ${event} gift!`);
+    };
+    return cards;
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown(x){
+    let countDown = x;
+    while (countDown >= 0){
+        console.log(countDown)
+        countDown --;
+    }
+}
+countDown(10);
